0e3f0c172f696d05c0dd7df924212809953b2cdf
AxleMaxGit <Spam@thefunkyway.com>
2013-07-30 20:51:48 +1000

    WIP

---
+++ 11_Lab_Review/simplefrom/app/controllers/students_controller.rb
@@ -0,0 +1,54 @@
+class StudentsController < ApplicationController
+
+    def index
+     @students = Student.all
+    end
+
+
+  def new
+    @student = Student.new
+    @students = Student.find(:all)
+  end
+
+  def create
+    @student = Student.new(params.require(:student).permit(:fname, :lname))
+    if @student.save
+    	redirect_to new_student_path
+    end
+  end
+
+  def show
+  	@student = Student.find(params[:id])
+  end
+
+
+#working area
+  def update
+    @student = Student.find(params[:id])
+
+		if @student.update_attributes permitted_attributes
+			redirect_to students_path
+		else
+			render 'edit'
+		end
+  end
+
+  def edit
+        @student = Student.find(params[:id])
+  end
+
+  def destroy
+
+    @student = Student.find(params[:id]).destroy
+    if @student.destroy
+    	redirect_to students_path
+    end
+  end
+
+	protected
+
+	def permitted_attributes
+		params.require(:student).permit(:fname, :lname)
+	end
+
+end
---
+++ 11_Lab_Review/simplefrom/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simplefrom::Application.config.secret_key_base = '9e670cf5db63dc920acead1765adb2168ddd6bd3b66fd470c53751bdd18282ec686b1aae63999f85b070e5bc7758ab7d5ca853c81dfe4d4494b2ad225e17085f'
